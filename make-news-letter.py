5e98cca7e2345349b989b6bf98235a516a5c40ad
Hyejung Hwang <seathat33@gmail.com>
2023-10-30 22:53:06 +0900

    fix:environment varialvle

--- make-news-letter.py
+++ make-news-letter.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
    
     dynamodb = boto3.resource(
         "dynamodb", 
-        region_name=os.getenv("AWS_REGION_NAME"),
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        region_name=os.environ("AWS_REGION_NAME"),
+        aws_access_key_id=os.environ("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ("AWS_SECRET_ACCESS_KEY")
     )
 
     table = dynamodb.Table('issueKeyword')
